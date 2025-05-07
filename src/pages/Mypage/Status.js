4d6b70a69c39b6f3b203880b6309c256022fb8a6
jungyeongmo7 <ballondor7@naver.com>
2025-05-07 10:18:27 +0900

    commit

--- src/pages/Mypage/Status.js
+++ src/pages/Mypage/Status.js
@@ -2,40 +2,40 @@ import React from "react";
 
 const Status = () => {
     return(
-        <div className="container-red"> {/*빨간박스*/}
-            <div className="box-yellow top">나의주문현황 {/*노란박스1*/}
+        <div className="status-container"> {/*빨간박스*/}
+            <div className="stauts-orderbox">나의주문현황 {/*노란박스1*/}
 
             </div>
-            <div className="box-yellow bottom"> {/*노란박스2*/}
-                <div className="box-green">
-                    <div className="box-blue">입금완료
+            <div className="status-bottombox"> {/*노란박스2*/}
+                <div className="status-subbox">
+                    <div className="status-subsubbox">입금완료
 
                     </div>
-                    <div className="box-blue">1
-
-                    </div>
-                </div>
-                <div className="box-green">
-                    <div className="box-blue">배송중
-
-                    </div>
-                    <div className="box-blue">10
+                    <div className="status-subsubbox">1
 
                     </div>
                 </div>
-                <div className="box-green">
-                    <div className="box-blue">배송완료
+                <div className="status-subbox">
+                    <div className="status-subsubbox">배송중
 
                     </div>
-                    <div className="box-blue">100
+                    <div className="status-subsubbox">10
 
                     </div>
                 </div>
-                <div className="box-green">
-                    <div className="box-blue">주문취소
+                <div className="status-subbox">
+                    <div className="status-subsubbox">배송완료
 
                     </div>
-                    <div className="box-blue">0
+                    <div className="status-subsubbox">100
+
+                    </div>
+                </div>
+                <div className="status-subbox">
+                    <div className="status-subsubbox">주문취소
+
+                    </div>
+                    <div className="status-subsubbox">0
 
                     </div>
                 </div>
